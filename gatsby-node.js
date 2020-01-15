1f165a51d4a3cf8256a06110b3296200257a4185
Vinicius Costa <viniciuscosta0197@gmail.com>
2020-01-15 16:43:37 -0300

    fix: change pagination number

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,7 +30,7 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
 
   const posts = result.data.allMarkdownRemark.edges
-  const postsPerPage = 10
+  const postsPerPage = 100
   const numPages = Math.ceil(posts.length / postsPerPage)
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
