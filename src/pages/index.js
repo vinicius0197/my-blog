0474e8e3dc71877ffcd3fc47253ffb2ef5d28043
Vinicius <viniciuscosta0197@gmail.com>
2022-04-23 15:50:00 -0300

    bump versions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import { Helmet } from 'react-helmet';
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 
-import Layout from '../components/layout/layout';
-import Wrapper from '../components/cover/cover';
-import MailCard from '../components/MailCard';
+import Layout from "../components/layout/layout"
+import Wrapper from "../components/cover/cover"
+import MailCard from "../components/MailCard"
 
 import {
   SinglePost,
@@ -12,14 +12,14 @@ import {
   PostDate,
   PostsContainer,
   About,
-  Title
-} from '../styles/styles';
+  Title,
+} from "../styles/styles"
 
-const MAXIMUM_POSTS = 5;
+const MAXIMUM_POSTS = 5
 
 class LastArticles extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
   render() {
     return (
@@ -32,20 +32,21 @@ class LastArticles extends React.Component {
         <h2>Recents Posts</h2>
 
         <div>
-          {this.props.posts.allMarkdownRemark.edges.slice(0, MAXIMUM_POSTS).map(({ node }) => (
-            <Link
-              to={node.fields.slug}
-            >
-              <SinglePost>
-                <PostTitle>
-                  {node.frontmatter.title}{" "} <PostDate> - {node.frontmatter.date}</PostDate>
-                </PostTitle>
-              </SinglePost>
-            </Link>
-          ))}
+          {this.props.posts.allMarkdownRemark.edges
+            .slice(0, MAXIMUM_POSTS)
+            .map(({ node }) => (
+              <Link to={node.fields.slug}>
+                <SinglePost>
+                  <PostTitle>
+                    {node.frontmatter.title}{" "}
+                    <PostDate> - {node.frontmatter.date}</PostDate>
+                  </PostTitle>
+                </SinglePost>
+              </Link>
+            ))}
         </div>
       </PostsContainer>
-    );
+    )
   }
 }
 
@@ -60,37 +61,35 @@ export default ({ data }) => {
       <Wrapper>
         <Title>Hi, I'm Vinicius</Title>
         <About>
-          I'm a software developer based in Brasilia, Brasil. You can find me at <a href="https://github.com/vinicius0197">Github</a> and check
-          my thoughts here at this blog.
+          I'm a software developer based in Brasilia, Brasil. You can find me at{" "}
+          <a href="https://github.com/vinicius0197">Github</a> and check my
+          thoughts here at this blog.
         </About>
       </Wrapper>
 
       <MailCard />
 
-      <LastArticles
-        posts={data}
-      >
-      </LastArticles>
+      <LastArticles posts={data}></LastArticles>
     </Layout>
-  );
+  )
 }
 
 export const query = graphql`
-{
-  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-    totalCount
-    edges {
-      node {
-        frontmatter {
-          title
-          date(formatString: "DD MMMM, YYYY")
+  {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
         }
-        fields {
-          slug
-        }
-        excerpt
       }
     }
   }
-}
-`;
+`
