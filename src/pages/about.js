0ffaa4e9f4cd5445a8f6e988c0a73ee4254e51be
Vinicius <viniciuscosta0197@gmail.com>
2022-08-07 10:25:15 -0300

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react"
+import { Helmet } from "react-helmet"
 
-import Layout from '../components/layout/layout';
-import {
-  AboutTitle,
-  AboutContainer,
-  AboutBody
-} from '../styles/styles';
+import Layout from "../components/layout/layout"
+import { AboutTitle, AboutContainer, AboutBody } from "../styles/styles"
 
 export default () => {
   return (
@@ -21,27 +17,14 @@ export default () => {
 
         <AboutBody>
           <p>
-          Hello! My name is Vinicius Costa and I'm a software developer. I currently study Computer Science at University of Brasilia
-          and work on cool side-projects that I would like to share here.
+            Hello! My name is Vinicius Costa and I'm a software developer. I'm
+            starting this blog as a way to document my learnings in software and
+            some other cool stuff that I care about. I think that writing about
+            things makes you better understand your own thought process, and
+            it's also a good way to document one's own knowledge.
           </p>
-
-          <p>
-          I'm starting this blog as a way to document my learnings in software and some other cool stuff that I care about. I think that
-          writing about things makes you better understand your own thought process, and it's also a good way to document one's own
-          knowledge and teach things.
-
-          </p>
-
-          <p>
-          I will be updating this section once I've done more stuff here. That's it for now!
-
-          </p>
-
-
-
         </AboutBody>
-
       </AboutContainer>
     </Layout>
-  );
+  )
 }
